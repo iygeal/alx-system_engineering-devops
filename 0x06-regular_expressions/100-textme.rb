3ecef9e1042bc3287b8e54cf1694360e23eef7e5
Innocent Anozie <iygeal@gmail.com>
2024-01-31 00:35:52 +0100

    Searching a logfile, matching paterns non-greedily and printing

--- 0x06-regular_expressions/100-textme.rb
+++ 0x06-regular_expressions/100-textme.rb
@@ -1,2 +1,13 @@
 #!/usr/bin/env ruby
+
+# ARGV[0]: Retrieves the first command-line argument passed to the script.
+# .scan(/\[from:(.*?)\] \[to:(.*?)\] \[flags:(.*?)\]/):
+#   - Uses regular expressions to search for patterns in the log entry.
+#   - \[from:(.*?)\]: Matches the 'from' part and captures content
+#	inside the brackets non-greedily.
+#   - \[to:(.*?)\]: Matches the 'to' part and captures content
+#	inside the brackets non-greedily.
+#   - \[flags:(.*?)\]: Matches the 'flags' part and captures
+#	content inside the brackets non-greedily.
+# .join(","): Joins the matched results using commas and prints the final result.
 puts ARGV[0].scan(/\[from:(.*?)\] \[to:(.*?)\] \[flags:(.*?)\]/).join(",")
