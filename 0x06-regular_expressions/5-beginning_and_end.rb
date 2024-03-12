9cb2e73e9a08d32196aa9f00ef0799ae060307b8
Innocent Anozie <iygeal@gmail.com>
2024-03-12 20:17:34 +0100

    Regex to match hn where a char not \n must appear in-between

---
+++ 0x06-regular_expressions/5-beginning_and_end.rb
@@ -0,0 +1,5 @@
+#!/usr/bin/env ruby
+puts ARGV[0].scan(/^h.n$/).join
+
+# Matches a string starting with h and ending with n
+# The string must have a character in-between h and n that is not a new line character
