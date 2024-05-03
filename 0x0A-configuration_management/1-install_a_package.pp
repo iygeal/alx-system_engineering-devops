e22fddf8e79162665fe5600255db10ded77b8116
Innocent Anozie <iygeal@gmail.com>
2024-05-03 21:49:07 +0100

    Refactored code to include orrect version of Werkzeug c

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,10 @@
-# Install flask package from pip3
+# Install Flask and Werkzeug packages from pip3
 package { 'Flask':
   ensure   => '2.1.0',
+  provider => 'pip3',
+}
+
+package { 'Werkzeug':
+  ensure   => '2.1.1',
   provider => 'pip3',
 }
