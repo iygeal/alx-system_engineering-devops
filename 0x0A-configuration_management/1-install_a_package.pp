bd0900829faf39a02702e70f95ba321e611a3460
Innocent Anozie <iygeal@gmail.com>
2024-05-03 14:40:38 +0100

    Install flask package

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,5 @@
-# Puppet manifest to install the package Flask, version 2.1.0 using pip3
+# Install flask package from pip3
 package { 'flask':
-ensure   => '2.1.0',
-provider => 'pip3',
+  ensure   => '2.1.0',
+  provider => 'pip3',
 }
