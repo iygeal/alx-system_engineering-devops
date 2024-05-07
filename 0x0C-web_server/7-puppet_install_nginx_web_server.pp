a3702e490d9352b3c975fca1798bf11e12fae859
Innocent Anozie <iygeal@gmail.com>
2024-05-08 00:03:54 +0100

    Refactored manifest

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,37 +1,20 @@
-# Install Nginx package
+# Install and configure nginx
 package { 'nginx':
   ensure => installed,
 }
 
-# Ensure Nginx service is running and enabled
-service { 'nginx':
-  ensure  => running,
-  enable  => true,
-  require => Package['nginx'],
+class { 'nginx':
+  manage_repo    => true,
+  package_source => 'nginx-stable',
 }
 
-# Define Nginx server configuration
-file { '/etc/nginx/sites-available/default':
-  ensure  => file,
-  content => template('nginx/default.erb'),
-  require => Package['nginx'],
+nginx::resource::server { 'default':
+  listen_port      => 80,
+  www_root         => '/var/www/html/',
+  vhost_cfg_append => { 'rewrite' => '^/redirect_me https://www.youtube.com/@naturethrillers/videos permanent' },
 }
 
-# Define the Nginx server configuration template
-file { '/etc/nginx/sites-available/default.erb':
-  ensure => present,
-  source => 'puppet:///modules/nginx/default.erb',
-}
-
-# Ensure Nginx is listening on port 80
-file { '/etc/nginx/sites-available/default':
+file { '/var/www/html/index.nginx-debian.html':
   ensure  => present,
-  content => "server {
-    listen 80;
-    server_name _;
-    location / {
-        return 301 https://www.youtube.com/@naturethrillers/videos;
-    }
-}",
-  notify  => Service['nginx'],
+  content => 'Hello World!',
 }
