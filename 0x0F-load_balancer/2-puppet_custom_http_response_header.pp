301a7402d6dca68f6f74fc9939e8c25b3f6a0247
Innocent Anozie <iygeal@gmail.com>
2024-05-14 00:04:36 +0100

    Puppet manifest to automate nginx config

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,50 +1,30 @@
-# Download and install Nginx web server
+# Setup New Ubuntu server with nginx
 # and add a custom HTTP header
 
 exec { 'update system':
-  command => '/usr/bin/apt-get update',
+        command => '/usr/bin/apt-get update',
 }
 
 package { 'nginx':
   ensure  => 'installed',
-  require => Exec['update system'],
+  require => Exec['update system']
 }
 
-file { '/var/www/html/index.html':
-  content => 'Hello World!',
+file {'/var/www/html/index.html':
+  content => 'Hello World!'
 }
 
-# Define nginx server block configuration
-file { '/etc/nginx/sites-available/default':
-  ensure  => present,
-  content => "
-server {
-    listen 80 default_server;
-    listen [::]:80 default_server;
-    root /var/www/html;
-    index index.html index.htm index.nginx-debian.html;
-    server_name _;
-    add_header X-Served-By ${facts['networking']['hostname']};
-    location / {
-        try_files \$uri \$uri/ =404;
-    }
-    if (\$request_filename ~ redirect_me){
-        rewrite ^ https://www.youtube.com/@naturethrillers/videos permanent;
-    }
-    error_page 404 /error_404.html;
-    location = /error_404.html {
-        internal;
-    }
-}",
+exec {'redirect_me':
+  command  => 'sed -i "24i\	rewrite ^/redirect_me https://www.youtube.com/@naturethrillers/videos/ permanent;" /etc/nginx/sites-available/default',
+  provider => 'shell'
 }
 
-exec { 'reload nginx':
-  command => 'service nginx reload',
-  require => File['/etc/nginx/sites-available/default'],
+exec {'HTTP header':
+  command  => 'sed -i "25i\	add_header X-Served-By \$hostname;" /etc/nginx/sites-available/default',
+  provider => 'shell'
 }
 
-service { 'nginx':
+service {'nginx':
   ensure  => running,
-  enable  => true,
-  require => Package['nginx'],
+  require => Package['nginx']
 }
