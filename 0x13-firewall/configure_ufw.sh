c4a32ebcfc3cdf8065b3b37385f22d7d74c7b61f
Innocent Anozie <iygeal@gmail.com>
2024-05-27 14:31:07 +0100

    Updated UFW config file and ran it on web-01

--- 0x13-firewall/configure_ufw.sh
+++ 0x13-firewall/configure_ufw.sh
@@ -1,4 +1,9 @@
+#!/usr/bin/env bash
+# Configure UFW to allow HTTP, HTTPS and SSH
+echo -e "Running system updates\n"
 sudo apt-get update -y
+
+echo -e "Installing UFW\n"
 sudo apt-get install ufw -y
 
 sudo ufw default deny in
@@ -9,3 +14,4 @@ sudo ufw allow 80/tcp
 sudo ufw allow 8080/tcp
 
 sudo ufw enable
+echo -e "Configuration completed successfully\n"
