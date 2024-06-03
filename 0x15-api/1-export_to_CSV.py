25b6cd806ec71d3a17de1659d65536a14f9d0506
Innocent Anozie <iygeal@gmail.com>
2024-06-03 17:47:57 +0100

    Extract from API, export CSV

---
+++ 0x15-api/1-export_to_CSV.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""Python script that gets from an API and exports to CSV"""
+
+
+import requests
+from sys import argv
+
+
+if __name__ == "__main__":
+    user_id = argv[1]
+    base_url = f"https://jsonplaceholder.typicode.com/users/{user_id}"
+
+    res = requests.get(base_url).json()
+    employee_name = res.get("name")
+
+    todo_url = f"{base_url}/todos"
+    tasks = requests.get(todo_url).json()
+
+    username = res.get("username")
+    task_status = "True" if tasks[0].get("completed") else "False"
+    task_title = tasks[0].get("title")
+
+    with open(f"{user_id}.csv", "w") as f:
+        for task in tasks:
+            f.write(f"{user_id},{username},{employee_name},
+                    {task_status},{task_title}\n")
