df27a4c99d445061c41db1516c6944bd726f1eb5
Innocent Anozie <iygeal@gmail.com>
2024-06-19 15:39:16 +0100

    Refactored code by using get() to fetch both 'data' and 'subscribers'

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,26 +1,23 @@
 #!/usr/bin/python3
 """Module that defines a function used to query Reddit API"""
 
-import requests
+from requests import get
 
 
 def number_of_subscribers(subreddit):
     """Returns the number of subreddit subscribers"""
-    if not isinstance(subreddit, str) or subreddit.strip() == "":
+    if not isinstance(subreddit, str) or subreddit is None:
         return 0
 
     url = f"https://www.reddit.com/r/{subreddit}/about.json"
-    headers = {"User-Agent": "Google Chrome Version 81.0.4044.129"}
+    headers = {"User-Agent": "python:app_iygeal:1.0.0 (by /u/iygeal)"}
+
+    response = get(url, headers=headers, allow_redirects=False)
+    if response.status_code != 200:
+        return 0
+    subs = response.json()
 
     try:
-        response = requests.get(url, headers=headers)
-        if response.status_code != 200:
-            return 0
-        else:
-            data = response.json()
-            subscribers = data["data"]["subscribers"]
-            return subscribers
-
-    except requests.RequestException as e:
-        print(f"Error fetching data for subreddit '{subreddit}': {e}")
+        return subs.get('data').get('subscribers')
+    except Exception:
         return 0
