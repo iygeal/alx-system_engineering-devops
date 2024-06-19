5e551ccec89a8a376934d57dfa8942a4bf5188b4
Innocent Anozie <iygeal@gmail.com>
2024-06-19 15:59:56 +0100

    Trying code refactors

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,27 +1,22 @@
 #!/usr/bin/python3
-"""
-number of subscribers for a given subreddit
-"""
+""""Module that defines a function used to query Reddit API"""
 
 from requests import get
 
 
 def number_of_subscribers(subreddit):
-    """
-    function that queries the Reddit API and returns the number of subscribers
-    (not active users, total subscribers) for a given subreddit.
-    """
-
+    """Returns the number of subreddit subscribers"""
     if subreddit is None or not isinstance(subreddit, str):
         return 0
 
-    user_agent = {'User-agent': 'Google Chrome Version 81.0.4044.129'}
-    url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
-    response = get(url, headers=user_agent)
-    results = response.json()
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    response = get(url)
+    if response.status_code != 200:
+        return 0
 
+    results = response.json()
     try:
-        return results.get('data').get('subscribers')
+        return results.get("data").get("subscribers")
 
     except Exception:
         return 0
